a27e86e4390aaafe70d3c926fd3c5887660b6df6
S <s>
2022-07-22 11:02:14 +0100

    Removed Amazon linux 2 support

--- open-vpn.tf
+++ open-vpn.tf
@@ -1,6 +1,4 @@
 resource "aws_instance" "open-vpn" {
-// uncomment if you want to use amazon linux 2 ami
-  # ami                          = data.aws_ami.linux2.id 
   ami                          = data.aws_ami.ubuntu.id
   instance_type                = var.instance_type
   subnet_id                    = aws_default_subnet.default_subnet.id
@@ -17,10 +15,7 @@ resource "aws_instance" "open-vpn" {
     volume_size = var.root_volume_size # in GB 
 
   }
-
-// uncomment if you want to use amazon linux 2
-  # user_data = file("${path.module}/userdata_amazon_linux2.tpl",
-  # )
+  
   user_data = file("${path.module}/userdata_ubuntu_arm.tpl",
   )
 
--- userdata_amazon_linux2.tpl
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo yum -y remove openvpn-as-yum
-sudo yum -y install https://as-repository.openvpn.net/as-repo-amzn2.rpm
-sudo yum -y install openvpn-as
